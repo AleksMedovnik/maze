84e3770b50a66a16ffdc4a62137557dd6281a11f
Aleksandr <medovnikaleksander@yandex.ua>
2021-06-01 15:13:27 +0300

    Added confetti of win.jsx

--- src/components/win/Win.jsx
+++ src/components/win/Win.jsx
@@ -3,8 +3,10 @@ import { NavLink } from "react-router-dom";
 export const Win = () => {
     return (
         <section className='win'>
-            <h1 className='title'>Вы вышли из лабиринта!!!</h1>
-            <NavLink to='/main' className='linkStart'>Играть еще раз</NavLink>
+            <div className=" win__main">
+                <h1 className='title'>Вы вышли из лабиринта!!!</h1>
+                <NavLink to='/main' className='linkStart'>Играть еще раз</NavLink>
+            </div>
         </section>
     )
 }
